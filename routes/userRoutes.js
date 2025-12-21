ec5c3fb1cb0d2b1db68e9019988f6beaa738a8b9
SoumyaGhosh2006 <soumyaraabghosh@gmail.com>
2025-12-22 00:52:30 +0530

    validateToken2.0

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,13 @@ const { registerUser,
         loginUser, 
         currentUser 
     } = require("../controllers/userController");
+const validateToken = require("../middleware/ValidateTokenHandler");
 const router = express.Router();
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
-router.post("/current", currentUser);
+router.post("/current", validateToken,  currentUser);
 
 module.exports= router;
