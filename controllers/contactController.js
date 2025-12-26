c49ceb77c44273f31e8e5f1acbb8b8a5974230a2
SoumyaGhosh2006 <soumyaraabghosh@gmail.com>
2025-12-27 02:26:23 +0530

    get all contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,17 +3,17 @@ const Contact = require("../models/contactModel")
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const getContacts= asyncHandler(async(req, res)=>{
-    const contacts= await Contact.find(); 
+    const contacts= await Contact.find({user_id : req.user.id}); 
     res.status(200).json(contacts);
 });
 
 
 //@desc Create all contacts
 //@route POST /api/contacts
-//@access public
+//@access private
 
 const createContact=asyncHandler(async(req, res)=>{
     console.log("Headers:", req.headers);
@@ -39,7 +39,7 @@ const createContact=asyncHandler(async(req, res)=>{
 
 //@desc Get all contacts
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 
 const getContact= asyncHandler(async(req, res)=>{
     const contact = await Contact.findById(req.params.id);
@@ -53,7 +53,7 @@ const getContact= asyncHandler(async(req, res)=>{
 
 //@desc Update all contacts
 //@route PUT /api/contacts/:id
-//@access public
+//@access private
 
 const updateContact= asyncHandler(async(req, res)=>{
     const contact = await Contact.findById(req.params.id);
@@ -73,8 +73,7 @@ const updateContact= asyncHandler(async(req, res)=>{
 
 //@desc Delete all contacts
 //@route DELETE /api/contacts/:id
-//@access public
-
+//@access private
 const deleteContact= asyncHandler(async(req, res)=>{
     const contact = await Contact.findById(req.params.id);
     if(!contact){
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,17 +5,13 @@ const {
     createContact,
     getContact,
     updateContact,
-    deleteContact,}= require("../controllers/contactController")
+    deleteContact,}= require("../controllers/contactController");
+const validateToken = require("../middleware/ValidateTokenHandler");
 
 
-router.route("/").get(getContacts);
+router.use(validateToken);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/").post(createContact);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports= router;
